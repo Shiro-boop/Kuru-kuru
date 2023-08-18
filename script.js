4bc6861f382e3e1344bcfd8d82a936602365bebf
Alex <137304331+Shiro-boop@users.noreply.github.com>
2023-08-19 01:38:00 +0300

    Add files via upload
    Новая версия сайта.
    Переработал функцию.Теперь она создаёт каждый раз новый объект в структуре сайта, после чего удаляя его.

--- script.js
+++ script.js
@@ -3,19 +3,23 @@
 let dataCount = document.querySelector(".data-count");
 let dataButton = document.querySelector(".data-button");
 let count = dataCount.getAttribute("data-count");
-let kuruId = document.getElementById('kuru-id')
 var kuruAudio = new Audio('kuruSound.mp3')
+let wrapper = document.querySelector('.wrapper');
+
+function AddHerta() {
+    let imgKuru = document.createElement('img');
+    imgKuru.src = 'KuruMov.gif'
+    wrapper.insertAdjacentElement('afterend', imgKuru);
+    imgKuru.classList.add('kuruClick')
+    setTimeout(() => {
+        imgKuru.remove();
+    }, 1300);
+};
 
 
 dataButton.addEventListener('click', function() {
-    kuruAudio.play();
+    AddHerta();
     count++;
-    kuruId.classList.add('kuruClick');
     dataCount.innerHTML = count;
     dataCount.setAttribute('data-count', count);
-    setTimeout(() => {
-        kuruId.classList.remove('kuruClick')
-        kuruAudio.load();
-    }, 1300);
 });
-
